97dc15002ce151107215c3db523267866d48004b
GaganGupta <gagan_gupta111@yahoo.com>
2018-12-06 01:18:56 +0530

    middleware, templating, moustache, hbs file as template and injecting data, rendering

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engines', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url} \n`;
+
+    console.log(log);
+    fs.appendFile('server.log', log, (err) => {
+        if(err){
+            console.log(' Unable to append file : server.log');
+        }
+    });
+    next();
+});
+
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('allCapitals', (message) => {
+    return message.toUpperCase();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'Maintenance page title set!'
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page title set!',
+        name: 'gagan',
+        likes: 32,
+        interests: [
+            'biking',
+            'cycling'
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page title set!'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle'
+    });
+})
+
+app.listen(3000);
